c8f0d0495d537870e403da4ec5d6c15a21dc54b0
Izajur <46420886+Izajur@users.noreply.github.com>
2020-11-26 20:59:04 +0530

    Update max_query.cpp

--- max_query.cpp
+++ max_query.cpp
@@ -11,14 +11,14 @@ void build_tree(int v, int tl, int tr, int arr[]) {
 		t[v] = max(t[2*v], t[2*v+1]);
 	}
 }
-int min_query(int v, int tl, int tr, int l, int r) {
+int max_query(int v, int tl, int tr, int l, int r) {
 	if(l>r)
 		return INT_MIN;
 	else if(l==tl and r==tr)
 		return t[v];
 	else {
 		int tm = (tl+tr)/2;
-		return max(min_query(2*v, tl, tm, l, min(r, tm)),
-					min_query(2*v+1, tm+1, tr, max(l, tm+1), r));
+		return max(max_query(2*v, tl, tm, l, min(r, tm)),
+					max_query(2*v+1, tm+1, tr, max(l, tm+1), r));
 	}
 }
